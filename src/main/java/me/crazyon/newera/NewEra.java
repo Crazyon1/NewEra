eb80a65a771d45161e60b678ca9dcb0c224ee027
Crazyon1 <Error909.exe@gmail.com>
2024-08-29 19:11:07 -0700

    aaa

--- src/main/java/me/crazyon/newera/NewEra.java
+++ src/main/java/me/crazyon/newera/NewEra.java
@@ -7,6 +7,6 @@ public class NewEra implements ModInitializer {
     public static final String  MOD_ID = "new_era";
     @Override
     public void onInitialize() {
-        NewEraItems.intialize(); // initalizes items
+        NewEraItems.intialize(); // initalizes items a
     }
 }
