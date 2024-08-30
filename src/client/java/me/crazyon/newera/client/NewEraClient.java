84c118ab6f96a90a6872ca7696cf4fcfb0581f2a
Crazyon1 <error909.exe@gmail.com>
2024-08-29 17:11:45 -0700

    aaa

---
+++ src/client/java/me/crazyon/newera/client/NewEraClient.java
@@ -0,0 +1,11 @@
+package me.crazyon.newera.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NewEraClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/me/crazyon/newera/NewEra.java
@@ -0,0 +1,10 @@
+package me.crazyon.newera;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NewEra implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
