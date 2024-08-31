99e8009ab0d96f50cdf92aaffbb415bc3038f35b
Crazyon1 <Error909.exe@gmail.com>
2024-08-30 18:23:21 -0700

    aaaadwad

--- src/main/java/me/crazyon/newera/NewEra.java
+++ src/main/java/me/crazyon/newera/NewEra.java
@@ -2,11 +2,16 @@ package me.crazyon.newera;
 
 import me.crazyon.newera.item.NewEraItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.dimension.DimensionType;
+
 
 public class NewEra implements ModInitializer {
     public static final String MOD_ID = "new_era";
     @Override
     public void onInitialize() {
         NewEraItems.initialize(); // initalizes items a
+
     }
 }
